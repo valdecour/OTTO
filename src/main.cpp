6aea8a1f87aece95d10e507ec7a53eece30a2114
maxi_minus <minus@Minus.home>
2020-06-06 00:01:24 +0200

    mon premier commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Start");
+
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("High");
+  // wait for a second
+  delay(500);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Low");
+   // wait for a second
+  delay(500);
+}
+
+
+
